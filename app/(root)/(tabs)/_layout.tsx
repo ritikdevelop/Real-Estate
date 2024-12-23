d0843a75ee53013d6ad50334a392b3e2f8a3b297
ritikdevelop <hrithikrana216@gmail.com>
2024-12-24 01:17:31 +0530

    feat: enhance tab layout and profile screen with new components and user settings

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,12 +1,90 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import icons from "@/constants/icons";
 
-const Tabs = () => {
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => {
   return (
-    <View>
-      <Text>Tabs</Text>
-    </View>
-  )
-}
+    <View className="flex-1 flex mt-3 flex-col items-center">
+      <Image
+        source={icon}
+        tintColor={focused ? "#0061ff" : "#666876"}
+        resizeMode="contain"
+        className="size-6"
+      />
 
-export default Tabs
+      <Text
+        className={`${
+          focused
+            ? "text-primary-300 font-rubik-medium"
+            : "text-black-200 font-rubik"
+        } text-xs w-full text-center mt-1`}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          borderTopColor: "#0061FF1A",
+          borderTopWidth: 1,
+          minHeight: 60,
+        },
+      }}
+    >
+      {/* //! Home Button Section */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.home} focused={focused} title="Home" />
+          ),
+        }}
+      />
+
+      {/* //! Explore Button Section */}
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.search} focused={focused} title="Explore" />
+          ),
+        }}
+      />
+
+      {/* //! Profile Button Section */}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.person} focused={focused} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,22 @@
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to Real Estate</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/[id]">Property</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Good Morning,</Text>
+              <Text className="text-base font-rubik-medium text-black-300">Lorem</Text>
+            </View>
+          </View>
+          
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
