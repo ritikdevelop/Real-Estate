6a17e455230c54b4098237e0b8f1d4f9614a83a8
ritikdevelop <hrithikrana216@gmail.com>
2025-01-06 12:24:47 +0530

    Refactor SignIn component to rename isLoggedIn to isLogged; update login handling and formatting

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,4 +1,11 @@
-import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
@@ -8,21 +15,20 @@ import { useGlobalContext } from "@/lib/global-provider";
 import { Redirect } from "expo-router";
 
 const SignIn = () => {
+  const { refetch, loading, isLogged } = useGlobalContext();
 
-    const { refetch, loading, isLoggedIn } = useGlobalContext();
-
-    if(!loading && isLoggedIn){
-        return <Redirect href="/" />;
-    }
+  if (!loading && isLogged) {
+    return <Redirect href="/" />;
+  }
 
   const handleLogin = async () => {
     const result = await login();
-    
-    if(result){
-        // console.log("Login Success");
-        refetch();
+
+    if (result) {
+      // console.log("Login Success");
+      refetch({});
     } else {
-        Alert.alert('Error', 'Failed to login');
+      Alert.alert("Error", "Failed to login");
     }
   };
 
